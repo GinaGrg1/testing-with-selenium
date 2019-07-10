457644e54872ee646c8b989fec64e44e8d3e8349
Regina Gurung <gina524286@gmail.com>
2019-07-10 23:13:05 +0100

    Add ChromeOptions. This is needed for chromedriver v75

--- tests/acceptance/steps/navigation.py
+++ tests/acceptance/steps/navigation.py
@@ -3,24 +3,19 @@ from selenium import webdriver
 
 
 use_step_matcher('re')
-
 options = webdriver.ChromeOptions()
-
-options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-#options.add_argument("--disable-extensions")
-#options.add_argument("--disable-gpu")
+options.add_experimental_option('w3c', False)
 
 
 @given('I am on the homepage')
 def step_impl(context):
-    context.browser = webdriver.Chrome('/usr/local/bin/chromedriver', chrome_options=options)
+    context.browser = webdriver.Chrome('/usr/local/bin/chromedriver', options=options)
     context.browser.get('http://127.0.0.1:5000')
 
 
 @given('I am on the blog page')
 def step_impl(context):
-    context.browser = webdriver.Chrome('/usr/local/bin/chromedriver', chrome_options=options)
+    context.browser = webdriver.Chrome('/usr/local/bin/chromedriver', options=options)
     context.browser.get('http://127.0.0.1:5000/blog')
 
 
